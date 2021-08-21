0c334c0ea0d33966240eabaaf3048a4f88fcaecb
shashank-bhati <64640244+shashank-bhati@users.noreply.github.com>
2021-08-21 17:04:40 +0530

    Update and rename parameter.c to Title.c

---
+++ 3_Implementation/src/Title.c
@@ -0,0 +1,16 @@
+/**
+ * @file parameter.c
+ * @author Shashank
+ * @brief to index of parameters of the machine
+ * @version 0.1
+ * @date 2021-07-07
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#include "../inc/wire.h"
+void Title(void)
+{
+   printf("\n\n*****************Selection of Multi-core and Single-core wire*******************\n\n");
+   
+}
