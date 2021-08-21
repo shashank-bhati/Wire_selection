238e11316269acc140db09c8743a26c46df83754
shashank-bhati <64640244+shashank-bhati@users.noreply.github.com>
2021-08-21 17:06:31 +0530

    Update and rename power_input.c to Factor_s.c

---
+++ 3_Implementation/src/Factor_s.c
@@ -0,0 +1,18 @@
+/**
+ * @file power_input.c
+ * @author Shashank
+ * @brief to check the  power_input of the machine
+ * @version 0.1
+ * @date 2021-07-07
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#include "../inc/wire.h"
+void Factor_s(void)
+{
+     printf("\n\n******The selection of  wire Depent on three Factors********\n\n");
+     printf("\n\n^^^^^^^^^^^Total Load^^^^^^^^^^^\n\n");
+     printf("\n\n^^^^^^^^^^^Total Current^^^^^^^^^^^\n\n");
+     printf("\n\n^^^^^^^^^^^Voltage Drop^^^^^^^^^^^\n\n");
+}
